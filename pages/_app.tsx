33263c0f30a44e0a1278a3f1af789a5eb3d93804
isaias-silva <zackblackmangold@gmail.com>
2022-12-15 17:58:43 -0300

    fluxo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
   const [response, setResponse] = useState('starting...')
   const [messages, setMessages]: Icontact[] | any = useState([])
   
-  const io = useSocket('http://localhost:8080', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNzE4NTEwMzZmZTUyZjEzZGJkYTZmZCIsIm5vbWUiOiJaYWNrIiwiY2xhc3NlIjoiYWRtIiwiZW1haWwiOiJpc2FpYXNnYXJyYWVsdXRhQGdtYWlsLmNvbSIsInZlcmlmeU1haWwiOnRydWUsImlhdCI6MTY3MDg1NjIzNSwiZXhwIjoxNjcxMTE1NDM1fQ.Yh_aAaZRvCznfN7fWdUc6qjuys9ch-UPNRSEKuS7CzE')
+  const io = useSocket('http://localhost:8080', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNzE4NTEwMzZmZTUyZjEzZGJkYTZmZCIsIm5vbWUiOiJaYWNrIiwiY2xhc3NlIjoiYWRtIiwiZW1haWwiOiJpc2FpYXNnYXJyYWVsdXRhQGdtYWlsLmNvbSIsInZlcmlmeU1haWwiOnRydWUsImlhdCI6MTY3MTExNTU1NSwiZXhwIjoxNjcxMzc0NzU1fQ.QmILt17bQLROyVfIDsLSOmiNQWKibWdbSjn2UZ0ZhGs')
 
   useEffect(() => {
 
