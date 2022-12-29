54def43ad6d1aebee8948d92cd6302e94ff2ee19
isaias-silva <zackblackmangold@gmail.com>
2022-12-29 16:02:16 -0300

    resolvido problema do buffer no socket.io

--- components/popup.tsx
+++ components/popup.tsx
@@ -3,49 +3,63 @@ import style from '../styles/Home.module.css'
 
 export default function Popup({ type, io }: any) {
     const [number, setNumber]: any = useState()
+    const [numbers, setNumbers] = useState('')
     const [message, setMessage]: any = useState()
 
-    const ioSend = () => {
-        alert(number)
-        alert(message)
-        if (!number || !message) {
-            return
+    const ioSend = (opt: 'message' | 'disparo' | 'flux') => {
+
+        if (opt == 'message') {
+
+            if (!number || !message) {
+                return
+            }
+            io.emit('sendText', {
+                type: 'text',
+                number: `${number}@s.whatsapp.net`,
+                text: message
+            })
+           
         }
-        io.emit('sendText', {
-            type: 'text',
-            number: `${number}@s.whatsapp.net`,
-            text: message
-        })
+        if (opt == 'disparo') {
+            if (!numbers || !message) {
+                return
+            }
+            const numbs = numbers.split(',').map((n) => n + '@s.whatsapp.net')
+            io.emit('disparo', {
+                numbers: numbs,
+                message
+            })
+        }
         setMessage(null)
-        
     }
 
 
 
     let component = <></>
 
-    const button = <button onClick={ioSend}>enviar</button>
     switch (type) {
         case 'message':
             component = <div>
 
                 <h3>{type}</h3>
                 <input type="number" placeholder='numero' onChange={(ev) => { setNumber(ev.target.value) }} />
-                <textarea placeholder='mensagem' value={message?message:''} onChange={(ev) => { setMessage(ev.target.value) }} >
+                <textarea placeholder='mensagem' value={message ? message : ''} onChange={(ev) => { setMessage(ev.target.value) }} >
 
                 </textarea>
-                {button}
+                <button onClick={() => { ioSend('message') }}>enviar</button>
+
             </div>
             break
         case 'disparo':
             component = <div>
                 <h3>{type}</h3>
                 <p>disparo de mensagem para vários numeros</p>
-                <input type="number" placeholder='numeros' />
-                <textarea placeholder='mensagem'>
+                <input onChange={(ev) => { setNumbers(ev.target.value) }} type="text" placeholder='numeros separe por virgula' />
+                <textarea placeholder='mensagem' value={message ? message : ''} onChange={(ev) => { setMessage(ev.target.value) }}>
 
                 </textarea>
-                {button}
+                <button onClick={() => { ioSend('disparo') }}>enviar</button>
+
             </div>
             break
         case 'flux':
@@ -56,7 +70,8 @@ export default function Popup({ type, io }: any) {
                 <textarea placeholder='mensagem'>
 
                 </textarea>
-                {button}
+                <button onClick={() => { ioSend('flux') }}>enviar</button>
+
             </div>
             break
         default:
