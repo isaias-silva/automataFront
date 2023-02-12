cd0e7a80dd39c618f8abfc36141121e4a943013a
isaias-silva <zackblackmangold@gmail.com>
2023-02-12 12:40:58 -0300

    usando grid e flexbox porq de margin não se vive

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -7,7 +7,14 @@ import Menu from './menu'
 import { useEffect, useState } from 'react'
 export default function Aside({ response, qr, messages, io }: { response: string, qr: string, messages: Icontact[], io: any }) {
         const [visible, setVisible] = useState(true)
-     
+        const [erase, setErase] = useState(false)
+        useEffect(() => {
+                if (visible == false) {
+                        setErase(true)
+                } else {
+                        setErase(false)
+                }
+        }, [visible])
         const treatRes = (res: string) => {
                 switch (res) {
                         case 'connected':
@@ -42,26 +49,42 @@ export default function Aside({ response, qr, messages, io }: { response: string
                                 img={item.picture || genericImage.src}
                                 memberImg={format?.picture || genericImage.src}
                                 size={item.msgs.length}
+                                visibleAside={erase}
                         />
                 }
         })
-        return <aside className={visible?style.aside:style.asideInvisible} >
+        return <aside className={visible ? style.aside : style.asideInvisible} >
+                {erase ? <>
+                        <button className={style.openBarButton} onClick={() => {
+                                const response = visible ? false : true
+                                setVisible(response)
+                        }}>&#8921;</button>
+                        <div className={style.image}>
+                                <img src={qr} alt={"qr"} className={treatRes(response)} />
+                        </div>
+                        <div className={style.messagesList}>
 
-                {response == 'connected' ? <Menu io={io} /> : null}
-                <button className={style.openBarButton} onClick={() => {
-                        const response = visible ? false : true
-                        setVisible(response)
-                }}>&#11013;</button>
-                <div className={style.image}>
-                        <img src={qr} alt={"qr"} className={treatRes(response)} />
+                                {msgComponents}
+                        </div>
+                </> : <>
+                        {response == 'connected' ? <Menu io={io} /> : null}
+                        <button className={style.closeBarButton} onClick={() => {
+                                const response = visible ? false : true
+                                setVisible(response)
+                        }}>&#11013;</button>
+                        <div className={style.image}>
+                                <img src={qr} alt={"qr"} className={treatRes(response)} />
 
-                </div>
-                <p>{response} {response == 'phone closed session' ? ', please reload' : null}</p>
+                        </div>
+                        <p>{response} {response == 'phone closed session' ? ', please reload' : null}</p>
 
-                <div className={style.messagesList}>
+                        <div className={style.messagesList}>
 
-                        {msgComponents}
-                </div>
+                                {msgComponents}
+                        </div>
+
+
+                </>}
 
         </aside>
 
--- components/Message.tsx
+++ components/Message.tsx
@@ -4,60 +4,62 @@ import { useState } from 'react'
 import style from '../styles/Home.module.css'
 export default function Message({
     number,
-   isCall,
+    isCall,
     text,
     img,
     memberImg,
     size,
-contactName,
-chatName,
-isGroup}: {
-    isCall:boolean,
-        chatName?:string,
+    contactName,
+    chatName,
+    isGroup,
+    visibleAside }: {
+        isCall: boolean,
+        chatName?: string,
         number: string,
         text: string,
         img?: string,
         memberImg?: string,
-        size:number,
-       contactName?:string
-       isGroup:boolean
+        size: number,
+        contactName?: string,
+        isGroup: boolean,
+        visibleAside: boolean
     }) {
-    const [call,setCall]=useState(isCall)
-    const [messagesNumb,setMessageNumb]=useState(size)
+    const [call, setCall] = useState(isCall)
+    const [messagesNumb, setMessageNumb] = useState(size)
     let filterText = text
     if (text.length > 50) {
         filterText = text.substring(0, 50) + '...'
     }
-    return <Link href={`/chat/${number}`} className={style.message} onClick={()=>{setMessageNumb(0)}}>
+    return < Link href={`/chat/${number}`} className={visibleAside ? style.messageTiny : style.message} onClick={() => { setMessageNumb(0) }}>
         <div className={style.perfil}>
-         {isGroup?<>
-         
-              <img src={img} alt="" />
-              {memberImg?<img src={memberImg} alt="member" className={style.member}/>:null}
-              {messagesNumb>1?<span className={style.numberMsgs}>{messagesNumb}</span>:null}
-           
-         
-         </>:
-         <>
-              <img src={img} alt="" />
-              {messagesNumb>1?<span className={style.numberMsgs}>{messagesNumb}</span>:null}   
-         </>
-         } 
 
-          
+            {isGroup ? <>
+                <img src={img} alt="" />
+                {memberImg ? <img src={memberImg} alt="member" className={style.member} /> : null}
+                {messagesNumb > 1 ? <span className={style.numberMsgs}>{messagesNumb}</span> : null}
+
+            </> :
+                <>
+                    <img src={img} alt="" />
+                    {messagesNumb > 1 ? <span className={style.numberMsgs}>{messagesNumb}</span> : null}
+                </>
+            }
+
+
         </div>
-        <div>
-            {isGroup?
-            <>
-            <h4>{chatName||'unamed'}:</h4>
-            <p>{contactName?<b>[{contactName}]: </b>:null}{filterText}</p>
-            </>:
-            <>
-            <h4>{chatName||'unamed'}:</h4>
-            <p>{text}</p>
-            
-            </>}
-            {call?<span className={style.warkingSpan} onClick={()=>{setCall(false)}}>  </span>:null}
-        </div>
-    </Link>
+        {visibleAside ? null : <div>
+            {isGroup ?
+                <>
+                    <h4>{chatName || 'unamed'}:</h4>
+                    <p>{contactName ? <b>[{contactName}]: </b> : null}{filterText}</p>
+                </> :
+                <>
+                    <h4>{chatName || 'unamed'}:</h4>
+                    <p>{text}</p>
+
+                </>}
+            {call ? <span className={style.warkingSpan} onClick={() => { setCall(false) }}>  </span> : null}
+        </div>}
+
+    </Link >
 }
